f6c36fff1fa7f31012749723e1ca845faa63b274
DavFilsDev <miharisoadavidfils@gmail.com>
2026-04-21 12:13:44 +0300

    feat(init): add defautl structure by maven and package used

---
+++ src/main/java/mg/federation/agricole/api/FederationApiApplication.java
@@ -0,0 +1,13 @@
+package mg.federation.agricole.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FederationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FederationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/federation/agricole/api/FederationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.federation.agricole.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FederationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
