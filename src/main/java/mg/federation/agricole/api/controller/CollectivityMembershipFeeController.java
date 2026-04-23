2aa608193972a0c0014a9b5a8c75a9a8657b3581
DavFilsDev <miharisoadavidfils@gmail.com>
2026-04-23 06:32:13 +0300

    feat(controller): add the endpoint for the /collectivities/{id}/membershipFees

--- src/main/java/mg/federation/agricole/api/controller/CollectivityMembershipFeeController.java
+++ src/main/java/mg/federation/agricole/api/controller/CollectivityMembershipFeeController.java
@@ -1,5 +1,6 @@
 package mg.federation.agricole.api.controller;
 
+import mg.federation.agricole.api.dto.CreateMembershipFee;
 import mg.federation.agricole.api.dto.MembershipFee;
 import mg.federation.agricole.api.service.MembershipFeeService;
 import org.springframework.http.ResponseEntity;
@@ -20,5 +21,13 @@ public class CollectivityMembershipFeeController {
     public ResponseEntity<List<MembershipFee>> getMembershipFees(@PathVariable String id) {
         List<MembershipFee> fees = membershipFeeService.getMembershipFees(id);
         return ResponseEntity.ok(fees);
+    }
+
+    @PostMapping("/collectivities/{id}/membershipFees")
+    public ResponseEntity<List<MembershipFee>> createMembershipFees(
+            @PathVariable String id,
+            @RequestBody List<CreateMembershipFee> createFees) {
+        List<MembershipFee> createdFees = membershipFeeService.createMembershipFees(id, createFees);
+        return ResponseEntity.ok(createdFees); // Selon la spec, retour 200, pas 201
     }
 }
